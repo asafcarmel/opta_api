ad303e90957dfa310f044bc672a66c32deb4454a
Asaf <asaf@fantasy65.com>
2017-04-02 17:47:08 +0300

    adjustments baseball

--- lib/opta/baseball/baseball_api.rb
+++ lib/opta/baseball/baseball_api.rb
@@ -4,6 +4,19 @@ module Opta
   module Baseball
     class BaseballApi < Opta::Api
 
+      def squads(tournament)
+        response_collection('squads', tmcl: tournament, _pgSz: 1000)
+      end
+
+      def match_list(tournament)
+        response_pages('match', tmcl: tournament)
+      end
+
+      def match_statistics(match)
+        resp = response_member('matchstats', match)
+        resp['matchStats']
+      end
+
       def path
         'baseballdata'
       end
