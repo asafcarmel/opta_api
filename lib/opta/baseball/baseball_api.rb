ef5590d2f64a779f709d8ed6cd6f33d59a361df8
Asaf <asaf@fantasy65.com>
2017-04-03 16:54:19 +0300

    permission added - no need to override

--- lib/opta/baseball/baseball_api.rb
+++ lib/opta/baseball/baseball_api.rb
@@ -4,17 +4,16 @@ module Opta
   module Baseball
     class BaseballApi < Opta::Api
 
-      def squads(tournament)
-        response_collection('squads', tmcl: tournament, _pgSz: 1000)
-      end
+      # def squads(tournament)
+      #   response_collection('squads', tmcl: tournament, _pgSz: 1000)
+      # end
 
-      def match_list(tournament)
-        response_pages('match', tmcl: tournament)
-      end
+      # def match_list(tournament)
+      #   response_pages('match', tmcl: tournament)
+      # end
 
       def match_statistics(match)
-        resp = response_member('matchstats', match)
-        resp['matchStats']
+        super['matchStats']
       end
 
       def path
